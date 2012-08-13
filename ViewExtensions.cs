ea101d673ba4c132725d90dc9f6363ac9c85522b
matt4446 <barmy_matt@hotmail.com>
2012-08-13 15:57:44 +0100

    model extensions

--- ViewExtensions.cs
+++ ViewExtensions.cs
@@ -11,44 +11,51 @@ namespace Orchard.NewtonsoftJson
 {
     public static class ViewExtensions
     {
-        public static HtmlString ToJsonDumpFromItems<TItem>(this HtmlHelper html, IEnumerable<TItem> items, bool useCamelCase = true)
+        public static HtmlString ToJsonDumpFromItems<TItem>(this HtmlHelper html, 
+            IEnumerable<TItem> items, 
+            bool useCamelCase = true,
+            bool formatJson = true)
         {
-            return html.ToJsonDump(items, useCamelCase);
+            return html.ToJsonDump(items, useCamelCase, formatJson);
         }
 
         public static HtmlString ToJsonDumpFromItems<TItem, TTransformedItem>(this HtmlHelper html, 
             IEnumerable<TItem> items, 
-            Func<TItem, TTransformedItem> transform, bool useCamelCase = true)
+            Func<TItem, TTransformedItem> transform, 
+            bool useCamelCase = true, 
+            bool formatJson = true)
         {
             var transformedItems = items.Select(e=> transform(e)).ToArray();
 
-            return html.ToJsonDump(transformedItems, useCamelCase);
+            return html.ToJsonDump(transformedItems, useCamelCase, formatJson);
         }
-
-
 
         public static HtmlString ToJsonDump<TModel>(this HtmlHelper html, 
             TModel model,
-            bool useCamelCase = true)
+            bool useCamelCase = true,
+            bool formatJson = true)
         {
             return new HtmlString(model.ToJson(useCamelCase));
         }
 
         public static HtmlString ToJsonDump<TModel, TTransform>(this HtmlHelper html, TModel model,
-            Func<TModel, TTransform> transform, bool useCamelCase = true)
+            Func<TModel, TTransform> transform, bool useCamelCase = true, bool formatJson = true)
         {
             var transformedItem = transform(model);
 
-            return new HtmlString(transformedItem.ToJson());
+            return new HtmlString(transformedItem.ToJson(useCamelCase: useCamelCase, formatJson: formatJson));
         }
 
-        private static string ToJson<TModel>(this TModel model, bool useCamelCase = true) 
+        public static string ToJson<TModel>(this TModel model, bool useCamelCase = true, bool formatJson = true) 
         {
             JsonSerializerSettings settings = useCamelCase ?
                 new JsonSerializerSettings() { ContractResolver = new CamelCasePropertyNamesContractResolver() } :
                 new JsonSerializerSettings() { };
 
-            return JsonConvert.SerializeObject(model, Formatting.Indented, settings);
+            return 
+                formatJson ?
+                JsonConvert.SerializeObject(model, Formatting.Indented, settings) : 
+                JsonConvert.SerializeObject(model, Formatting.None, settings);
         }
     }
 }
